ebabcea0a30296b36c60c776ba3d517e0b382068
Monster Hunter: Terra <45815732+simo631m@users.noreply.github.com>
2018-12-12 10:54:52 +0100

    Update Gunlance.cs

--- Items/Gunlance.cs
+++ Items/Gunlance.cs
@@ -18,7 +18,7 @@ namespace MonsterHunterTerra.Items
 			item.height = 40;
 			item.useTime = 20;
 			item.useAnimation = 20;
-			item.useStyle = 1;
+			item.useStyle = 3;
 			item.knockBack = 6;
 			item.value = 10000;
 			item.rare = 2;
