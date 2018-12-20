b5ca637a148e472e5942e68fe754afb2a4c2fd11
Monster Hunter: Terra <45815732+simo631m@users.noreply.github.com>
2018-12-20 10:42:55 +0100

    Add files via upload

---
+++ MonsterHunterTerra/Items/Tools/MachaliteAxe.cs
@@ -0,0 +1,71 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using ReLogic.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.GameContent;
+using Terraria.GameContent.Achievements;
+using Terraria.GameContent.Events;
+using Terraria.GameContent.Tile_Entities;
+using Terraria.GameContent.UI;
+using Terraria.GameInput;
+using Terraria.Graphics.Capture;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.IO;
+using Terraria.Localization;
+using Terraria.ObjectData;
+using Terraria.Social;
+using Terraria.UI;
+using Terraria.UI.Chat;
+using Terraria.UI.Gamepad;
+using Terraria.Utilities;
+using Terraria.World.Generation;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace MonsterHunterTerra.Items.Tools
+{
+	public class MachaliteAxe : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Machalite Axe");
+			Tooltip.SetDefault("\n[c/FF0000:Tier 1 Axe ]");
+		}
+
+		public override void SetDefaults() {
+			item.damage = 10;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 15;
+			item.useAnimation = 30;
+			item.axe = 18;
+			item.useStyle = 1;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 3;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+        public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(null, "Bar1", 13);
+			recipe.AddTile(16);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+    
+		}
+
+	}
+}
