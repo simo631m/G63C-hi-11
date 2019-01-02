e76253e4f1190e2dd92b326caeb7cde43424ec17
Monster Hunter: Terra <45815732+simo631m@users.noreply.github.com>
2019-01-02 06:04:37 +0100

    Add files via upload

--- MonsterHunterTerra/MonsterHunterTerraWorld.cs
+++ MonsterHunterTerra/MonsterHunterTerraWorld.cs
@@ -16,16 +16,6 @@ namespace MonsterHunterTerra
 {
     public class MonsterHunterTerraWorld : ModWorld
     {
-        private void GenOre1Tile(GenerationProgress progress)
-        {
-            progress.Message = "Generation Machalite";
-
-            for (int k = 0; k < (int)((double)(Main.maxTilesX * Main.maxTilesY) * 6E-05); k++)
-            {
-                int i2 = WorldGen.genRand.Next(0, Main.maxTilesX);
-                int j2 = WorldGen.genRand.Next((int)(Main.maxTilesY * .3f), (int)(Main.maxTilesY * .45f));
-                WorldGen.OreRunner(i2, j2, WorldGen.genRand.Next(3, 4), WorldGen.genRand.Next(3, 8), (ushort)mod.TileType("Ore1Tile"));
-            }
-        }
+       public static bool spawnOre = false;
     }
 }
